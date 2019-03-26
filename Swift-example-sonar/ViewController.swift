b2674f8fa80367a48bff1fc9156657708adbaf36
Aarón BM <aaronspooky@users.noreply.github.com>
2019-03-25 18:21:31 -0600

    First commit

---
+++ Swift-example-sonar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-example-sonar
+//
+//  Created by Aaron on 3/25/19.
+//  Copyright © 2019 Aaron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
